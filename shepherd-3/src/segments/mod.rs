b1105a6ad152094991d015022899396739a68f8a
bjackson312006 <blakejackson312006@gmail.com>
2026-08-26 17:03:40 -0500

    shepherd-3: added chips module, started voltages module

--- shepherd-3/src/segments/mod.rs
+++ shepherd-3/src/segments/mod.rs
@@ -11,6 +11,93 @@ embassy_stm32::bind_interrupts!(struct Irqs {
     GPDMA1_CHANNEL3 => embassy_stm32::dma::InterruptHandler<embassy_stm32::peripherals::GPDMA1_CH3>;
 });
 
+mod chips {
+    /// ID for each ADBMS6830 chip.
+    #[repr(usize)]
+    #[derive(variant_count::VariantCount)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    #[derive(defmt::Format)]
+    pub enum ChipId {
+        /// Segment 0, Alpha Chip
+        Chip0,
+        /// Segment 0, Beta Chip
+        Chip1,
+        /// Segment 1, Alpha Chip
+        Chip2,
+        /// Segment 1, Beta Chip
+        Chip3,
+        /// Segment 2, Alpha Chip
+        Chip4,
+        /// Segment 2, Beta Chip
+        Chip5,
+        /// Segment 3, Alpha Chip
+        Chip6,
+        /// Segment 3, Beta Chip
+        Chip7,
+        /// Segment 4, Alpha Chip
+        Chip8,
+        /// Segment 4, Beta Chip
+        Chip9
+    }
+    impl ChipId {
+        /// Whether a chip is Alpha or Beta.
+        pub const fn kind(&self) -> ChipKind {
+            if ((*self as usize) % 2) == 0 {
+                ChipKind::Alpha
+            } else {
+                ChipKind::Beta
+            }
+        }
+
+        /// Whether or not this chip is Alpha.
+        pub const fn is_alpha(&self) -> bool {
+            matches!(self.kind(), ChipKind::Alpha)
+        }
+
+        /// Whether or not this chip is Beta.
+        pub const fn is_beta(&self) -> bool {
+            matches!(self.kind(), ChipKind::Beta)
+        }
+
+        /// Indicates what segment this chip is on.
+        pub const fn segment(&self) -> SegmentId {
+            match self {
+                ChipId::Chip0 => SegmentId::Segment0,
+                ChipId::Chip1 => SegmentId::Segment0,
+                ChipId::Chip2 => SegmentId::Segment1,
+                ChipId::Chip3 => SegmentId::Segment1,
+                ChipId::Chip4 => SegmentId::Segment2,
+                ChipId::Chip5 => SegmentId::Segment2,
+                ChipId::Chip6 => SegmentId::Segment3,
+                ChipId::Chip7 => SegmentId::Segment3,
+                ChipId::Chip8 => SegmentId::Segment4,
+                ChipId::Chip9 => SegmentId::Segment4,
+            }
+        }
+    }
+
+    /// The type of the chip (Alpha or Beta).
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    #[derive(variant_count::VariantCount)]
+    #[derive(defmt::Format)]
+    pub enum ChipKind {
+        Alpha,
+        Beta,
+    }
+
+    /// ID for each segment.
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    #[derive(variant_count::VariantCount)]
+    #[derive(defmt::Format)]
+    pub enum SegmentId {
+        Segment0,
+        Segment1,
+        Segment2,
+        Segment3,
+        Segment4,
+    }
+}
+
 mod alias {
     use embedded_hal_bus::spi::ExclusiveDevice;
     use embassy_time::Delay;
@@ -22,7 +109,7 @@ mod alias {
     use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 
     /// Number of ADBMS6830B chips we have.
-    const ADBMS6830B_NUM_CHIPS: usize = 10;
+    const ADBMS6830B_NUM_CHIPS: usize = const { super::chips::ChipId::VARIANT_COUNT };
 
     /// Type alias representing a SPI controller that implements `SpiDevice` from `embedded_hal_async`.
     /// This is just a single SPI controller with a CS pin.
--- shepherd-3/src/segments/voltages.rs
+++ shepherd-3/src/segments/voltages.rs
@@ -2,6 +2,10 @@
 
 impl super::Segments {
     pub async fn read_cell_voltages(&self) {
+        use adbms6830b::chip::registers::results::
+
         let service = &self.service;
+        
+        service.read<>()
     }
 }
